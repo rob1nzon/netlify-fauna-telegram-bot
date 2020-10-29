88d30125004dd6c241df4e65c808f842a5174249
Denis <chentsov@restream.rt.ru>
2020-10-29 15:39:06 +0300

    Kek

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -3,29 +3,48 @@ const startAction = require('./actions/start')
 var request = require('request');
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+const {MenuTemplate, MenuMiddleware} = require('telegraf-inline-menu')
+
+// const menuTemplate = new MenuTemplate(ctx, msg => `${msg}`)
+
+// menuTemplate.interact('¯\_(ツ)_/¯', 'a', {})
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getTik(t) {
+  console.log(t);
+  var options = {
+    url: 'https://query2.finance.yahoo.com/v7/finance/options/'+t};
+    function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+        tiker=JSON.parse(body);
+        return tiker['optionChain']['result'][0]['quote']['shortName']+' '+tiker['optionChain']['result'][0]['quote']['regularMarketPrice'];
+    }
+    else {
+      return "None";
+    }
+    }
+    request(options, callback);
+}
 
 bot.start(ctx => {
   return startAction(ctx)
 })
-bot.command('oldschool', (ctx) => ctx.reply('Hello man'))
+
+answer = ['поку-ку-ку-ку-пай','продавай', 'шортим', '¯\_(ツ)_/¯']
+bot.command('petu', (ctx) => ctx.reply(answer[getRandomInt(0,answer.length)]))
 bot.command('t', (ctx) => {
   console.log(ctx.message.text);
-  var options = {
-  url: 'https://query2.finance.yahoo.com/v7/finance/options/'+ctx.message.text.split(' ')[1]};
-  function callback(error, response, body) {
-  if (!error && response.statusCode == 200) {
-      console.log(body);
-      tiker=JSON.parse(body);
-      ctx.reply(tiker['optionChain']['result'][0]['quote']['shortName']+' '+tiker['optionChain']['result'][0]['quote']['regularMarketPrice']);
-      
-  }
-  else {
-    ctx.reply(response.statusCode);
-  }
-  }
-  request(options, callback);
-
+  ctx.reply(' '+getTik(ctx.message.text.split(' ')[1]))
 })
+// const menuMiddleware = new MenuMiddleware('/', menuTemplate)
+// bot.command('tt', ctx => menuMiddleware.replyToContext(ctx, '123'))
+// bot.use(menuMiddleware)
 
 exports.handler = async event => {
   try {
